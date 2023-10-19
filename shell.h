a729d06f4cc41bd3be341b98c8cb3832806481fa
mosescode1 <emosescode@gmail.com>
2023-10-19 08:05:53 +0100

    pheww 2

--- shell.h
+++ shell.h
@@ -6,16 +6,12 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#define MY_EOF (-1)
 int _putchar(char chararter);
 int custom_printf(const char *output_string);
 void display_prompt(void);
 void command_executor(const char *command);
 void read_user_command(char **command, size_t *size);
 void print_env(char **env);
-void _cd_check(char *commands);
-ssize_t _getline(char **lineptr, size_t *n, int fd);
+ssize_t _getline(char **buffer, size_t *static_size);
 #endif
